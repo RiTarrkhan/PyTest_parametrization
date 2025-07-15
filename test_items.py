9dffdfe3048b129f0ddac6241c0b1c550795c6a7
Виктория Палешева <103785111+RiTarrkhan@users.noreply.github.com>
2025-07-15 18:32:30 +0300

    Update test_items.py
    Fix bag

--- test_items.py
+++ test_items.py
@@ -9,6 +9,6 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 def test_add_to_basket_button(browser):
     browser.get(link)
     time.sleep(30)
-    res_test =  browser.find_element(By.CSS_SELECTOR, ".btn.btn-lg.btn-primary.btn-add-to-basket") 
+    res_test =  browser.find_elements(By.CSS_SELECTOR, ".btn.btn-lg.btn-primary.btn-add-to-basket") 
     assert res_test, "button doesn't exist"
     
